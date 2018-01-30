188bc41d5b197d9d044fba5ad2fa1660b74baed1
Stav Davis <stav.davis@gmail.com>
2018-01-30 11:37:10 -0800

    allowing netlify to access api (cors)

--- politismart-serverside/server.js
+++ politismart-serverside/server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 // CORS handling via middleware: /////////////////////////////////////////////
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://politismart.netlify.com');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   if (req.method === 'OPTIONS') {
