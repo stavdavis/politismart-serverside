118818c2d8da2e68ffdd80777acdd7ea15eafac5
Stav Davis <stav.davis@gmail.com>
2018-01-30 12:00:04 -0800

    added general cors access

--- politismart-serverside/server.js
+++ politismart-serverside/server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 // CORS handling via middleware: /////////////////////////////////////////////
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   if (req.method === 'OPTIONS') {
     return res.send(204);
